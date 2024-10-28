20a337a6056258eb41e2949f075c1a9e2d7dc442
RyanProbeAcc <rcp228@cornell.edu>
2024-10-28 13:19:49 -0400

    Create utils.py

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,17 @@
+# Copy and paste your OpenAI API Key
+openai_api_key = "<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
+>"
+# Put your name
+key_owner = "<RyanProbe>"
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose 
+debug = True
